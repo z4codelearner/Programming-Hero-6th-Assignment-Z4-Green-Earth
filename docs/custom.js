834c448c0d87b101d9a1cad0043431aecabeac90
Abu Zafor <z4codelearn@gmail.com>
2025-09-09 22:42:06 +0600

    All tree btn display all trees is done

--- docs/custom.js
+++ docs/custom.js
@@ -36,3 +36,54 @@ const loadAllPlants = () => {
       displayPlants(allPlants.plants);
     });
 };
+
+// All tree btn display all trees
+
+document
+  .getElementById("all-trees-btn")
+  .addEventListener("click", loadAllPlants);
+
+//*display trees/plants
+
+const displayPlants = (plants) => {
+
+  const plantsCardContainer = document.getElementById("plants-card-container");
+  plantsCardContainer.innerHTML = "";
+  for (let plant of plants) {
+    const plantCard = document.createElement("div");
+    plantCard.innerHTML = `
+    <div class="card bg-base-100 h-[600px] p-1 shadow-sm">
+              <figure>
+                <img
+                  class="h-[250px] w-full "
+                  src="${plant.image}"
+                  alt=""
+                />
+              </figure>
+              <div class="card-body p-2">
+                <h2 onclick="loadTreeDetails(${plant.id})" class="card-title">${plant.name}</h2>
+                <p class="text-gray-500">
+                  ${plant.description}
+                </p>
+                <div class="card-actions justify-between items-center">
+                  <div class="btn rounded-full bg-green-200 text-green-600">
+                    ${plant.category}
+                  </div>
+                  <div class="font-semibold">
+                    <i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}
+                  </div>
+                </div>
+                <div>
+                  <button onclick="displayCart(this)"
+                    class=" bg-green-600 w-full rounded-full p-2 text-white cursor-pointer"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+    `;
+    plantsCardContainer.append(plantCard);
+  }
+  manageSpinner(false);
+};
